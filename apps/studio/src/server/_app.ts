9a8da6e680accf1ebc99b23ab14ff9fcfb1da61f
Blankeos <carloantonioct@gmail.com>
2026-03-18 05:49:07 +0800

    feat: cors 5555 for security.

--- apps/studio/src/server/_app.ts
+++ apps/studio/src/server/_app.ts
@@ -4,7 +4,16 @@ import { spacetimeController } from "./modules/spacetime/spacetime.controller"
 
 const app = new Hono()
 
-app.use(cors())
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:5555",
+      "http://127.0.0.1:5173",
+      "http://127.0.0.1:5555",
+    ],
+  })
+)
 
 export const appRouter = app.route("/spacetime", spacetimeController)
 
